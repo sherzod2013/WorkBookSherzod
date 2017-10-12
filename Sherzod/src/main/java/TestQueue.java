4c03923dfd79d3db798a9ad6081665354520d416
Sherzod <sherzod2013_exe@mail.ru>
2017-10-12 22:57:40 +0500

    Set

--- Sherzod/src/main/java/TestQueue.java
+++ Sherzod/src/main/java/TestQueue.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by Sherzod on 12/10/2017.
@@ -21,5 +19,93 @@ public class TestQueue {
         strings.addAll(collection);
 
         System.out.println(strings);
+
+        Set<Integer>  numbers  = new HashSet<Integer>();
+        numbers.addAll(Arrays.asList(new Integer[] {1,2,3,4,5,6,7,8,9}));
+
+        numbers.add( 1);
+        System.out.println(numbers);
+        Set<Integer> numbers2 = new SortedSet<Integer>() {
+            public Comparator<? super Integer> comparator() {
+                return null;
+            }
+
+            public SortedSet<Integer> subSet(Integer fromElement, Integer toElement) {
+                return null;
+            }
+
+            public SortedSet<Integer> headSet(Integer toElement) {
+                return null;
+            }
+
+            public SortedSet<Integer> tailSet(Integer fromElement) {
+                return null;
+            }
+
+            public Integer first() {
+                return null;
+            }
+
+            public Integer last() {
+                return null;
+            }
+
+            public int size() {
+                return 0;
+            }
+
+            public boolean isEmpty() {
+                return false;
+            }
+
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            public Iterator<Integer> iterator() {
+                return null;
+            }
+
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            public boolean add(Integer integer) {
+                return false;
+            }
+
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            public boolean addAll(Collection<? extends Integer> c) {
+                return false;
+            }
+
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            public void clear() {
+
+            }
+        };
+        numbers2.addAll(Arrays.asList(new Integer[] {1,2,5,2,3,7,3,1,8,0,8,9,6,5}));
+        System.out.println(numbers2.toArray());
+        Collections.addAll(collection, numbers2.toArray());
+        System.out.println(collection);
+
     }
 }
