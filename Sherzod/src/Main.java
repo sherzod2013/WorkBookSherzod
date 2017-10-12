237b2ff66cc4b247111401a1fec0e3da16ac02a2
Sherzod <sherzod2013_exe@mail.ru>
2017-10-12 19:39:55 +0500

    Add list

--- Sherzod/src/Main.java
+++ Sherzod/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Sherzod on 12/10/2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        List<> es  = new LinkedList<E>();
+
+    }
+
 }
